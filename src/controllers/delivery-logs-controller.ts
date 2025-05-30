ddea6cfc1c971edefa1a8ce0a1198928c59cf0a1
Vinícius Gomes <161797273+ViniciusGGomes@users.noreply.github.com>
2025-05-30 16:56:16 +0000

    feat: add delivery logs creation and retrieval

--- src/controllers/delivery-logs-controller.ts
+++ src/controllers/delivery-logs-controller.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from "express";
-import { tuple, z } from "zod";
+import { z } from "zod";
 import { prisma } from "../database/prisma";
 import { AppError } from "@/utils/AppError";
 
